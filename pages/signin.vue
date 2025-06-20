13d7a95d106a3c3feb5f0bae8e73448ae4e79c11
juswa2000 <joshuadejesus017@gmail.com>
2025-06-20 10:35:46 +0800

    Added login page

---
+++ pages/signin.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class=" d-flex justify-center align-center"  style="height: 100vh;">
+        <v-card max-width="500">
+        <v-card-text>
+            <v-form>
+                <h1 style="text-align: center;">Log In</h1>
+                <br>
+                <v-text-field label="Email" prepend-inner-icon="mdi-email" outlined></v-text-field>
+                <v-text-field label="Password" type="password" prepend-inner-icon="mdi-lock" outlined></v-text-field>
+                <v-btn  :loading="loading" color="blue" size="large" type="submit" variant="elevated" block> Sign In</v-btn>
+            </v-form>
+        </v-card-text>
+        </v-card>
+    </div>
+</template>
+
+<script>
+export default{
+    layout: "auth",
+}
+</script>
